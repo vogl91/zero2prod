945e08a69983a1dd903a2d2caa76462a2fc718cc
Alexander Vogelgsang <vogel91@gmx.de>
2022-01-08 11:45:23 +0100

    minimal setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8000")?;
+    zero2prod::run(listener)?.await
 }
